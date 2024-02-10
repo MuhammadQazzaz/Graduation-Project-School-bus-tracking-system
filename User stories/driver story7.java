86cdf893bc2bd708d9277a10512215e211b9019f
Muhammad El qazzaz <104651472+MuhammadQazzaz@users.noreply.github.com>
2024-02-10 15:50:20 +0200

    Update driver story7.java

--- User stories/driver story7.java
+++ User stories/driver story7.java
@@ -1,1 +1,38 @@
+public class Driver {
+    private boolean loggedIn;
 
+
+
+// Other properties and methods specific to the Driver class...
+
+public boolean isLoggedIn() {
+    return loggedIn;
+}
+
+public void login() {
+    // Implement login logic
+    loggedIn = true;
+    System.out.println("Driver logged in");
+}
+
+public void logout() {
+    // Implement logout logic
+    if (loggedIn) {
+        loggedIn = false;
+        System.out.println("Driver logged out securely");
+    } else {
+        System.out.println("Driver is not logged in");
+    }
+}
+
+// Additional methods and properties...
+
+public static void main(String[] args) {
+    Driver driver = new Driver();
+    driver.login();
+
+    // Perform tasks...
+
+    driver.logout();
+}
+}
